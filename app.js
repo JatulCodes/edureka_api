432c863db6254246727dd6a588cff404d9dbf447
JatulCodes <69075503+JatulCodes@users.noreply.github.com>
2021-02-16 14:53:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.get('/orders', (req, res) => {
 //mongodb server connections
 MongoClient.connect(mongourl, (err, connection) => {
     if (err) console.log(err);
-    db = connection.db('edureka_intern');
+    db = connection.db('restaurent');
     app.listen(port, (err) => {
         if (err) throw err;
         console.log(`Server is running on port ${port}`)
